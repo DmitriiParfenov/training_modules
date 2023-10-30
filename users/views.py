3ce838e2dbe04dcd51810b28bc9dfb528d20ce41
Dmitrii <dima12sik@yandex.ru>
2023-10-30 20:49:33 +0300

    added CRUD views for the User model

--- users/views.py
+++ users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+from users.models import User
+from users.permissions import IsAuthenticatedOrIsOwner
+from users.serializers import UserListSerializer, UserSerializer
+
 
 # Create your views here.
+class UserListAPIView(generics.ListAPIView):
+    """Для получения объектов модели User."""
+    queryset = User.objects.all()
+    serializer_class = UserListSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    """Для изменения информации объектов модели User."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticatedOrIsOwner,)
+
+
+class UserDetailAPIView(generics.RetrieveAPIView):
+    """Для получения детализированной информации объектов модели User."""
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticatedOrIsOwner,)
+
+
+class UserDeleteAPIview(generics.DestroyAPIView):
+    """Для удаления объектов модели User."""
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = (IsAdminUser,)
