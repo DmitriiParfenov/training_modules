2bc2452c15979c83626499a182aaaa756769b709
Dmitrii <dima12sik@yandex.ru>
2023-10-30 21:27:48 +0300

    registered Module model in the django admin panel

--- modules/admin.py
+++ modules/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from modules.models import Module
+
+
 # Register your models here.
+@admin.register(Module)
+class ModuleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description')
+    list_display_links = ('id',)
+    search_fields = ('title',)
+    list_filter = ('title',)
