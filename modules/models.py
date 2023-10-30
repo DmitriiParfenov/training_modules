7e58b0afc9f4ff1d86d8aaf6036d6f567c0b9b44
Dmitrii <dima12sik@yandex.ru>
2023-10-30 21:27:27 +0300

    added Module model

--- modules/models.py
+++ modules/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Module(models.Model):
+    title = models.CharField(max_length=30, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Модуль'
+        verbose_name_plural = 'Модули'
