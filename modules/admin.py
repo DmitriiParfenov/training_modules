36330abf7619bc5bdc7b4d7b52a575c66cc827c8
Dmitrii <dima12sik@yandex.ru>
2023-11-05 19:48:06 +0300

    added module_user field in the django admin panel

--- modules/admin.py
+++ modules/admin.py
@@ -6,7 +6,7 @@ from modules.models import Module
 # Register your models here.
 @admin.register(Module)
 class ModuleAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'description')
+    list_display = ('id', 'title', 'description', 'module_user')
     list_display_links = ('id',)
     search_fields = ('title',)
     list_filter = ('title',)
